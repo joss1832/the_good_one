6412397d30d99ce302c1f0a167fa396b68e1848c
Josue <21387996@uagro.mx>
2025-04-08 13:25:58 -0600

    subiendo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 
-const btn_pedir = document.querySelector('#mostrar');
+const btn_pedir = document.querySelector('#pedir');
 let alumno_info = [];
 btn_pedir.addEventListener('click', function(event){
+    event.preventDefault()
     let nombre = prompt('cual es tu nombre?');
     let grupo = prompt('cual es tu grupo?');
     let matricula = prompt('cual es tu matricula?');
@@ -12,11 +13,27 @@ btn_pedir.addEventListener('click', function(event){
         matricula:matricula
     })
 
-    let info = document.createElement('div')
-    info.innerHTML = `
-    <p>nombre: ${nombre}</p>
-    <p>grupo : ${grupo}</p>
-    <p>matricula : ${matricula}</p>`
     
-    document.getElementById('#contenedor_info').appendChild(info)
 });
+
+const btn_mostrar = document.querySelector('#alumnos');
+btn_mostrar.addEventListener('click', function(e){
+    e.preventDefault();
+
+    const contenedor = document.querySelector('#contenedor_info');
+    contenedor.innerText = '';
+
+    for (let i = 0; i < alumno_info.length; i++) {
+        let alumno = alumno_info[i];
+        let info = document.createElement('div');
+        info.innerText =
+        `nombre: ${alumno.nombre}
+         grupo: ${alumno.grupo}
+         matricula: ${alumno.matricula}
+        `
+
+        contenedor.appendChild(info)
+        document.getElementById("contenedor_info").style.display = "flex";
+
+    }
+})
